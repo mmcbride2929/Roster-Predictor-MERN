68b408ff07d3f26c2017c4fec69afa4958378943
Michael McBride <Michaelmcbridefilms@gmail.com>
2023-01-09 22:48:31 -0500

    change fetch path after hosting

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -17,7 +17,9 @@ const Home = () => {
 
   useEffect(() => {
     const fetchRoster = async () => {
-      const response = await axios.get(`http://localhost:5000/api/v1/roster`)
+      const response = await axios.get(
+        `https://roster-predictor-mern-production.up.railway.app/api/v1/roster`
+      )
 
       setAllPlayers(response.data)
       setLoading(false)
